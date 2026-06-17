1ca8a689d5eded97ee7eae124da3871c3e6b3202
johnfrancisguinto <74706947+johnfrancisguinto@users.noreply.github.com>
2026-06-17 10:17:56 +0800

    Correct private key formatting in credentials
    Fix formatting of private key in credentials dictionary

--- multi_product_dashboard.py
+++ multi_product_dashboard.py
@@ -101,6 +101,10 @@ scope = [
 ]
 
 creds_dict = json.loads(os.environ["GOOGLE_CREDENTIALS"])
+
+
+creds_dict["private_key"] = creds_dict["private_key"].replace("\\n", "\n")
+
 creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
 client = gspread.authorize(creds)
 
