afb3f9dcefeb49d1c5730a860f0d96ce8a9c86e2
johnfrancisguinto <74706947+johnfrancisguinto@users.noreply.github.com>
2026-06-17 09:39:50 +0800

    Update credentials loading for Google Sheets API

--- multi_product_dashboard.py
+++ multi_product_dashboard.py
@@ -96,8 +96,10 @@ scope = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
 ]
-creds = Credentials.from_service_account_file("imi-creds.json", scopes=scope)
-client = gspread.authorize(creds)
+
+creds_dict = json.loads(os.environ["GOOGLE_CREDENTIALS"])
+creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
+
 
 # ================= UTIL =================
 def load_sheet(sheet_name):
